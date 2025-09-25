b1b340956e5edd3ecd877500be2b8678cd87eead
BearWithMe18 <noahfau1@gmail.com>
2025-09-25 16:37:57 -0400

    Select fix

--- grange-calc/src/app/app.ts
+++ grange-calc/src/app/app.ts
@@ -1,14 +1,22 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Score } from './score/score';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Score],
+  imports: [RouterOutlet, Score, ReactiveFormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('grange-calc');
+
+  itemCategories = signal(["None", "Animal Products", "Artisan Goods", "Cooking", "Fish", "Foraging", "Fruits", "Minerals", "Vegetables"]);
+
+  itemForm = new FormGroup({
+    item1: new FormControl(this.itemCategories()[0]),
+  })
+  
   public scoreVal = 14;
 }
