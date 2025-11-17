99f89b390e5c137238fc481c6be3e39e3bfdda7b
BearWithMe18 <noahfau1@gmail.com>
2025-11-16 22:55:57 -0500

    Reconfigured grange item

--- grange-calc/src/app/app.ts
+++ grange-calc/src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Score } from './score/score';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import categoryData from '../data/categories.json';
+import { GrangeItem } from './grange-item/grange-item';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Score, ReactiveFormsModule],
+  imports: [RouterOutlet, Score, ReactiveFormsModule, GrangeItem],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
@@ -16,23 +17,6 @@ export class App {
 
   itemCategories = ["None", "Animal Products", "Artisan Goods", "Cooking", "Fish", "Forage", "Fruits", "Minerals", "Vegetables"];
 
-  itemForm = new FormGroup({
-    item1: new FormControl(this.itemCategories[0]),
-    item2: new FormControl(this.itemCategories[0]),
-    item3: new FormControl(this.itemCategories[0]),
-    item4: new FormControl(this.itemCategories[0]),
-    item5: new FormControl(this.itemCategories[0]),
-    item6: new FormControl(this.itemCategories[0]),
-    item7: new FormControl(this.itemCategories[0]),
-    item8: new FormControl(this.itemCategories[0]),
-    item9: new FormControl(this.itemCategories[0]),
-  })
-
   public scoreVal = 14;
   $number: any;
-
-  category(_index: number) {
-    var temp = this.itemForm.get('item' + (_index))?.value;
-    return categoryData[temp as keyof typeof categoryData];
-  }
 }
--- grange-calc/src/app/grange-item/grange-item.ts
+++ grange-calc/src/app/grange-item/grange-item.ts
@@ -1,4 +1,6 @@
 import { Component, input, model, signal } from '@angular/core';
+import categoryData from '../../data/categories.json';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-grange-item',
@@ -7,7 +9,29 @@ import { Component, input, model, signal } from '@angular/core';
   styleUrl: './grange-item.css'
 })
 export class GrangeItem {
+  categoryData = categoryData;
+  itemNum = input(1);
   itemCategories = input<string[]>();
   itemScore = model<number>();
+  item = "";
+  itemName = "";
+
+  setItem(item: any){
+    this.item = item.target.value;
+    this.itemName = "";
+  }
+
+  setItemName(item: any){
+    this.itemName = item.target.value;
+  }
+
+  category(item: string) {
+    return categoryData[item as keyof typeof categoryData];
+  }
+
+  form = new FormGroup({
+    itemCat: new FormControl(this.item),
+    itemName: new FormControl(this.itemName),
+  });
 
 }
